0b7d2beb2c332af1c9f8bab7570b42bd73fdcbdb
Vladimir Popchenko <pwd70@mail.ru>
2023-08-04 23:31:33 +0300

    change code

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,22 +1,44 @@
 from gendiff.generate_diff import generate_diff
 import json
+import pytest
 
-def test_file_download():
-    file_path1 = json.load(open('file1.json'))
-    assert file_path1 == {
+@pytest.fixture
+
+def file_path1():
+    return {
   "host": "hexlet.io",
   "timeout": 50,
   "proxy": "123.234.53.22",
-  "follow": False
+  "follow": false
 }
 
+def file_path2():
+    return {
+  "timeout": 20,
+  "verbose": true,
+  "host": "hexlet.io"
+}
+
+#def test_file_download():
+   # file_path1 = json.load(open('file1.json'))
+   # assert file_path1 == {
+ # "host": "hexlet.io",
+ # "timeout": 50,
+ # "proxy": "123.234.53.22",
+  #"follow": False
+#}
 
 
-def test_generate_diff():   
-    result = generate_diff('file3.json', 'file4.json')
-    assert result == {'  test': 'one'}
+
+#def test_generate_diff():   
+   # result = generate_diff('file3.json', 'file4.json')
+    #assert result == {'  test': 'one'}
 
    
+#def test_generate_diff2():   
+    #result = generate_diff(file_path1, file_path2)
+    #assert result == {'- follow': False, '  host': 'hexlet.io', '- proxy': '123.234.53.22', '- timeout': 50, '+ timeout': 20, '+ verbose': True}
+
 def test_generate_diff2():   
-    result = generate_diff('file1.json', 'file2.json')
+    result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
     assert result == {'- follow': False, '  host': 'hexlet.io', '- proxy': '123.234.53.22', '- timeout': 50, '+ timeout': 20, '+ verbose': True}
