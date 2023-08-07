d63342b4706e836b82565e876da5d240246bd5f4
Vladimir Popchenko <pwd70@mail.ru>
2023-08-07 14:51:55 +0300

    change

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -47,6 +47,6 @@ def test_generate_diff2():
     assert result == {'- follow': False, '  host': 'hexlet.io', '- proxy': '123.234.53.22', '- timeout': 50, '+ timeout': 20, '+ verbose': True}
 
 #def test_format_dict_to_string():
-    result2 = test_dict = format_dict_to_string({'one':'two'})
+    result2 = test_dict = format_dict_to_string({'one': 'two'})
 
-    assert result2 == "{'one':'two'}"
+    assert result2 == "{'one': 'two'}"
